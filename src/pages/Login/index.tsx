a37924b57c6082fc9826c2f332aff1b166a227fa
matihp <matiasleoncontreras@gmail.com>
2023-01-05 19:01:14 -0300

    change login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
+import { useNotification } from "../../context/notification.context";
+import { LoginValidate } from "../../utils/validateForm";
 
 
 export const LoginPage: React.FC<{}>=()=>{
+
+  const {getError,getSuccess}=useNotification()
+
   type LoginType={
     username:string;
     password:string;
@@ -16,7 +21,12 @@ export const LoginPage: React.FC<{}>=()=>{
   }
   const handleSubmit=(e:React.ChangeEvent<HTMLInputElement>)=>{
     e.preventDefault()
-    console.log(loginData)
+    LoginValidate.validate(loginData).then(()=>{
+      getSuccess(JSON.stringify(loginData))
+    }).catch((error)=>{
+      getError(error.message)
+    })
+    
   }
     return(
     <Container maxWidth="sm">
@@ -25,8 +35,8 @@ export const LoginPage: React.FC<{}>=()=>{
             <Paper sx={{padding:"1.2em",borderRadius:"0.5em"}}>
                 <Typography variant="h4"sx={{mt:"1vh",mb:"1vh"}}>Iniciar sesion</Typography>
                 <Box component='form' onSubmit={handleSubmit}>
-                    <TextField name='username' type='text' fullWidth label="Email" sx={{mt:"2vh",mb:"1.5vh"}}margin='normal' required onChange={dataLogin}/>
-                    <TextField name='password' type='password' fullWidth label= "Password" sx={{mt:"1.5vh",mb:"1.5vh"}}margin='normal' required onChange={dataLogin}/>
+                    <TextField name='username' type='text' fullWidth label="Email" sx={{mt:"2vh",mb:"1.5vh"}}margin='normal'  onChange={dataLogin}/>
+                    <TextField name='password' type='password' fullWidth label= "Password" sx={{mt:"1.5vh",mb:"1.5vh"}}margin='normal' onChange={dataLogin}/>
                     <Button fullWidth type="submit"sx={{mt:"1.5vh",mb:"3vh"}} variant='contained'>Iniciar sesion</Button>
                 </Box>
             </Paper>
