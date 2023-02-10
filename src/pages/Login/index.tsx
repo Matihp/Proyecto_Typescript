3be12382b431288fcbfce46548186c648b740305
var2s <125094836+var2s@users.noreply.github.com>
2023-02-10 19:59:13 -0300

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -28,17 +28,22 @@ export const LoginPage: React.FC<{}>=()=>{
     <Container maxWidth="sm">
       <Grid container direction="column" justifyContent='center' alignItems='center' sx={{minHeight:"100vh"}}>
         <Grid item>
-            <Paper sx={{padding:"1.2em",borderRadius:"0.5em"}}>
+            <Paper 
+              sx={{padding:"1.2em",borderRadius:"0.5em"}}>
                 <Typography variant="h4"sx={{mt:"1vh",mb:"1vh"}}>Iniciar sesion</Typography>
                 <Box component='form' onSubmit={formik.handleSubmit}>
-                    <TextField name='username' type='text' fullWidth label="Email" sx={{mt:"2vh",mb:"1.5vh"}}margin='normal' value={formik.values.username}
+                    <TextField 
+                        name='username' type='text' fullWidth label="Email" sx={{mt:"2vh",mb:"1.5vh"}}margin='normal' value={formik.values.username}
                         onChange={formik.handleChange}
                         error={formik.touched.username && Boolean(formik.errors.username)}
-                        helperText={formik.touched.username && formik.errors.username}/>
-                    <TextField name='password' type='password' fullWidth label= "Password" sx={{mt:"1.5vh",mb:"1.5vh"}}margin='normal' value={formik.values.password}
+                        helperText={formik.touched.username && formik.errors.username}
+                      />
+                    <TextField 
+                        name='password' type='password' fullWidth label= "Password" sx={{mt:"1.5vh",mb:"1.5vh"}}margin='normal' value={formik.values.password}
                         onChange={formik.handleChange}
                         error={formik.touched.password && Boolean(formik.errors.password)}
-                        helperText={formik.touched.password && formik.errors.password}/>
+                        helperText={formik.touched.password && formik.errors.password}
+                      />
                     <Button fullWidth type="submit"sx={{mt:"1.5vh",mb:"3vh"}} variant='contained'>Iniciar sesion</Button>
                 </Box>
             </Paper>
